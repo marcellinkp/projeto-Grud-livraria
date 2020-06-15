dd0daa975964afc26632dbe36047df9917bc9a64
marcellinkp <65822871+marcellinkp@users.noreply.github.com>
2020-06-15 13:52:05 -0300

    Update D.B.php

--- D.B.php
+++ D.B.php
@@ -3,6 +3,6 @@
 date_default_timezone_set('America/Sao_Paulo');
 
 define('DB_SERVIDOR','localhost');
-define('DB_USUARIO','levi');
+define('DB_USUARIO','Marcel sampaio');
 define('DB_SENHA', '123');
 define('DB_BANCO','livraria');
